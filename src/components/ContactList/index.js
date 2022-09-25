94b50d03473b2912b0e40aab3dbbe9e03f7f07cb
Bruno Lopes <eusoubruno7@gmail.com>
2022-09-25 20:14:50 -0300

    add Card component

--- src/components/ContactList/index.js
+++ src/components/ContactList/index.js
@@ -1,4 +1,9 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactList() {
   return (
@@ -7,6 +12,68 @@ export default function ContactList() {
         <strong>3 Contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </header>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Bruno Lopes</strong>
+              <small>instagram</small>
+            </div>
+            <span>eusoubruno7@gmail.com</span>
+            <span>(41 99999-9999)</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Bruno Lopes</strong>
+              <small>instagram</small>
+            </div>
+            <span>eusoubruno7@gmail.com</span>
+            <span>(41 99999-9999)</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Bruno Lopes</strong>
+              <small>instagram</small>
+            </div>
+            <span>eusoubruno7@gmail.com</span>
+            <span>(41 99999-9999)</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="trash" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
--- src/components/ContactList/styles.js
+++ src/components/ContactList/styles.js
@@ -10,7 +10,6 @@ export const Header = styled.header`
   justify-content: space-between;
   strong {
     font-size: 24px;
-    color: #222;
   }
   a {
     color: ${({ theme }) => theme.colors.primary.main};
@@ -23,6 +22,72 @@ export const Header = styled.header`
     &:hover {
       background: ${({ theme }) => theme.colors.primary.main};
       color: #FFF
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+  header {
+    margin-bottom: 8px;
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+    }
+
+    span {
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #FFF;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  padding: 16px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        text-transform: uppercase;
+        font-weight: bold;
+        padding: 4px;
+        border-radius: 4px;
+        margin-left: 8px;
+      }
+    }
+
+    span {
+      display: block;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 8px;
     }
   }
 `;
