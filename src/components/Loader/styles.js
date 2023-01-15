893dd01f3407501c5c6fa6ff8fcfac026bec7fae
Bruno Lopes <eusoubruno7@gmail.com>
2023-01-15 10:23:08 -0300

    small fix

--- src/components/Loader/styles.js
+++ src/components/Loader/styles.js
@@ -45,20 +45,20 @@ export const Overlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-.loader {
-  color: ${({ theme }) => theme.colors.primary.main};
-  font-size: 90px;
-  text-indent: -9999em;
-  overflow: hidden;
-  width: 1em;
-  height: 1em;
-  border-radius: 50%;
-  margin: 72px auto;
-  position: relative;
-  -webkit-transform: translateZ(0);
-  -ms-transform: translateZ(0);
-  transform: translateZ(0);
-  -webkit-animation: ${load} 1.7s infinite ease, ${round} 1.7s infinite ease;
-  animation: ${load} 1.7s infinite ease, ${round} 1.7s infinite ease;
-}
+  .loader {
+    color: ${({ theme }) => theme.colors.primary.main};
+    font-size: 90px;
+    text-indent: -9999em;
+    overflow: hidden;
+    width: 1em;
+    height: 1em;
+    border-radius: 50%;
+    margin: 72px auto;
+    position: relative;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation: ${load} 1.7s infinite ease, ${round} 1.7s infinite ease;
+    animation: ${load} 1.7s infinite ease, ${round} 1.7s infinite ease;
+  }
 `;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,13 +5,10 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import Modal from '../../components/Modal';
-import Loader from '../../components/Loader';
 
 export default function Home() {
   return (
     <Container>
-      <Loader />
       <InputSearchContaier>
         <input type="text" placeholder="Pesquisar nome..." />
       </InputSearchContaier>
