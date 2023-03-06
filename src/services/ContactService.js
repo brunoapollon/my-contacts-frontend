1f78a8b71fa09a7d02f0898997a8344b45b682fc
Bruno Lopes <eusoubruno7@gmail.com>
2023-03-06 09:22:39 -0300

    small fix

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -10,7 +10,7 @@ class ContactService {
   }
 
   async createContact(contact) {
-    return this.httpClient.post('/contacts', contact);
+    return this.httpClient.post('/contacts', { body: contact });
   }
 }
 
