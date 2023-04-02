bf7fbd84a2dcc2fcbad72cec66bd41f1f04477c0
Bruno Lopes <eusoubruno7@gmail.com>
2023-04-02 10:47:01 -0300

    refactor useErrors

--- src/hooks/useErrors.js
+++ src/hooks/useErrors.js
@@ -1,25 +1,23 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export default function useErrors() {
   const [errors, setErrors] = useState([]);
 
-  function setError({ field, message }) {
+  const setError = useCallback(({ field, message }) => {
     const errorAlreadyExists = errors.find((error) => error.field === field);
 
     if (errorAlreadyExists) return;
 
     setErrors((prevState) => [...prevState, { field, message }]);
-  }
+  }, [errors]);
 
-  function removeError(fieldName) {
+  const removeError = useCallback((fieldName) => {
     setErrors((prevState) => prevState.filter((error) => error.field !== fieldName));
-  }
+  }, []);
 
-  function getErrorMessageByFieldName(fieldName) {
-    const errorFound = errors.find((error) => error.field === fieldName);
-
-    return errorFound ? errorFound.message : '';
-  }
+  const getErrorMessageByFieldName = useCallback((fieldName) => {
+    setErrors((prevState) => prevState.filter((error) => error.field !== fieldName));
+  }, []);
 
   return {
     errors,
